8562d1f870496e307778b70f8d09bc21ee4ee72c
pascalbothner <pascal.bothner@outlook.de>
2022-08-04 18:51:56 +0200

    initial commit

---
+++ apps/brainrecord/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'studyapp-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'brainrecord';
+}
